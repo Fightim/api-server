051a6f0f8f63c7802fa504e8bcc3e66decdcabac
qkrwjdan <madogisa12@naver.com>
2022-10-08 02:36:58 +0900

    [feat] user create시 중복 email 검사

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from 'src/users/dto';
@@ -9,6 +9,13 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
+    const isExists = await this.userModel.findOne({
+      email: createUserDto.email,
+    });
+    if (isExists) {
+      throw new BadRequestException('이미 사용중인 이메일입니다.');
+    }
+
     const createdUser = this.userModel.create(createUserDto);
     return createdUser;
   }
