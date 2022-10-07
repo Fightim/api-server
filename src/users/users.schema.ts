8e34bac42204d0eef7feed03b06f2b04aecd37a3
qkrwjdan <madogisa12@naver.com>
2022-10-08 01:18:36 +0900

    [feat] user schema 초안 작성

---
+++ src/users/users.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: number;
+
+  @Prop()
+  accessKey: string;
+
+  @Prop()
+  secret: string;
+
+  @Prop()
+  instances: [mongoose.Schema.Types.ObjectId];
+
+  @Prop()
+  loadBalancers: [mongoose.Schema.Types.ObjectId];
+
+  @Prop()
+  rds: [mongoose.Schema.Types.ObjectId];
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  loginAt: [Date];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
