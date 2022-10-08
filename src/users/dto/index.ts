dea853481300d9c4895f480f6c74af18e99dbf86
qkrwjdan <madogisa12@naver.com>
2022-10-08 13:06:29 +0900

    [feat] RegisterUserKeyDto 생성

--- src/users/dto/index.ts
+++ src/users/dto/index.ts
@@ -1,5 +1,6 @@
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UserResponseDto } from 'src/users/dto/user-response.dto';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
+import { RegisterUserKeyDto } from 'src/users/dto/register-user-key.dto';
 
-export { CreateUserDto, UserResponseDto, LoginUserDto };
+export { CreateUserDto, UserResponseDto, LoginUserDto, RegisterUserKeyDto };
