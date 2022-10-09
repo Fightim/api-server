0192a6d71afc69a89e5a537f3f6aa34668628eaa
qkrwjdan <madogisa12@naver.com>
2022-10-10 01:31:50 +0900

    [feat] load-balancers API 초안 작성

--- src/load-balancers/load-balancers.controller.ts
+++ src/load-balancers/load-balancers.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 
 @Controller('load-balancers')
-export class LoadBalancersController {}
+export class LoadBalancersController {
+  @Get()
+  async getLoadBalancers() {
+    return 'GET /load-balancers';
+  }
+
+  @Get(':loadBalancerId')
+  async getLoadBalancer() {
+    return 'GET /load-balancers/:loadBalancerId';
+  }
+
+  @Post()
+  async createLoadBalancer() {
+    return 'POST /load-balancers';
+  }
+
+  @Delete(':loadBalancerId')
+  async deleteLoadBalancer() {
+    return 'DELETE /load-balancers/:loadBalancerId';
+  }
+}
