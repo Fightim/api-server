63e25285c1d665561ac5373392eebf6c118444e5
qkrwjdan <madogisa12@naver.com>
2022-10-10 00:52:52 +0900

    [docs] POST /instances, DELETE /instances/:instanceId API 명세서 작성

---
+++ src/instances/docs/swagger.ts
@@ -0,0 +1,39 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+} from '@nestjs/swagger';
+import { CreateInstanceDto } from 'src/instances/dto';
+import { InstanceResponseDto } from 'src/instances/dto/instance-response.dto';
+
+export function CreateInstanceDocs() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '인스턴스 생성하기',
+    }),
+    ApiBody({
+      type: CreateInstanceDto,
+    }),
+    ApiOkResponse({
+      type: InstanceResponseDto,
+    }),
+  );
+}
+
+export function DeleteInstanceDocs() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '인스턴스 삭제하기',
+    }),
+    ApiParam({
+      name: 'instanceId',
+      description: '삭제할 인스턴스의 Id',
+      type: String,
+    }),
+    ApiOkResponse({
+      type: Boolean,
+    }),
+  );
+}
--- src/instances/instances.controller.ts
+++ src/instances/instances.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Delete, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  CreateInstanceDocs,
+  DeleteInstanceDocs,
+} from 'src/instances/docs/swagger';
 
+@ApiTags('Instance')
 @Controller('instances')
 export class InstancesController {
   @Post()
+  @CreateInstanceDocs()
   async create() {
     return 'POST /instances';
   }
 
   @Delete(':instanceId')
+  @DeleteInstanceDocs()
   async delete() {
     return 'DELETE /instances/:instanceId';
   }
