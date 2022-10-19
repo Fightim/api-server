49020562bd025a114a7ffa4943f1be8724116bc8
qkrwjdan <madogisa12@naver.com>
2022-10-19 23:13:42 +0900

    [docs] api 명세서 description 수정

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -2,8 +2,16 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const appVersion = '1.0';
 
+const description = `
+The Cloud-GUI API description  
+화이팀 API 서버 명세서입니다.  
+개발 BaseUrl : http://52.79.126.134:3000/  
+`;
+
 export const swaggerConfig = new DocumentBuilder()
+  .addServer('http://localhost:3000')
+  .addServer('http://52.79.126.134:3000/')
   .setTitle('Cloud-GUI')
-  .setDescription('The Cloud-GUI API description')
+  .setDescription(description)
   .setVersion(appVersion)
   .build();
