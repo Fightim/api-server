777d0834b4246bb9473f0d0afe9fe0c0625e8a82
qkrwjdan <madogisa12@naver.com>
2022-11-30 23:58:14 +0900

    [docs] 개발 서버 url 설명 삭제

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -5,7 +5,6 @@ export const appVersion = '1.0';
 const description = `
 The Cloud-GUI API description  
 화이팀 API 서버 명세서입니다.  
-개발 BaseUrl : http://causeapiserver-env.eba-pegczwgz.ap-northeast-2.elasticbeanstalk.com/api-docs/  
 `;
 
 export const swaggerConfig = new DocumentBuilder()
