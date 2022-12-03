36e292d33010459887814c2c3c494fa112cc9cbd
qkrwjdan <madogisa12@naver.com>
2022-12-03 17:01:03 +0900

    [feat] RdsResponse의 속성들 전부 Optional로 변경

--- src/rds/dto/rds-response.dto.ts
+++ src/rds/dto/rds-response.dto.ts
@@ -6,14 +6,14 @@ export class RdsOptions {
     required: true,
     type: String,
   })
-  name: string;
+  name?: string;
 
   @ApiProperty({
     description: '마스터 사용자 이름',
     required: true,
     type: String,
   })
-  masterUserName: string;
+  masterUserName?: string;
 }
 
 export class RdsInformations {
@@ -22,35 +22,35 @@ export class RdsInformations {
     required: true,
     type: String,
   })
-  id: string;
+  id?: string;
 
   @ApiProperty({
     description: '데이터베이스의 상태',
     required: true,
     type: String,
   })
-  DBInstanceStatus: string;
+  DBInstanceStatus?: string;
 
   @ApiProperty({
     description: '엔드포인트',
     required: true,
     type: String,
   })
-  endPoint: string;
+  endPoint?: string;
 
   @ApiProperty({
     description: '포트',
     required: true,
     type: Number,
   })
-  port: number;
+  port?: number;
 
   @ApiProperty({
     description: '스토리지',
     required: true,
-    type: String,
+    type: Number,
   })
-  storage: string;
+  storage?: number;
 }
 
 export class RdsResponseDto {
