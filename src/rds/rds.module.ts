942dd61cb0c80e8db5384227239593c4f80105ec
qkrwjdan <madogisa12@naver.com>
2022-12-06 03:00:03 +0900

    [feat] rds module에 rdsSchema 추가

--- src/rds/rds.module.ts
+++ src/rds/rds.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Rds, RdsSchema } from 'src/rds/schemas/rds.schema';
 import { UsersModule } from 'src/users/users.module';
 import { RdsController } from './rds.controller';
 import { RdsService } from './rds.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forFeature([{ name: Rds.name, schema: RdsSchema }]),
+  ],
   controllers: [RdsController],
   providers: [RdsService],
 })
