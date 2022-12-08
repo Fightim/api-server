1fbae87495bdf4ec83f6cce8288487c20653bca5
qkrwjdan <madogisa12@naver.com>
2022-12-08 13:53:59 +0900

    [chore] instance dto import 수정

--- src/instances/dto/index.ts
+++ src/instances/dto/index.ts
@@ -1,4 +1,9 @@
 import { CreateInstanceDto } from 'src/instances/dto/create-instance.dto';
 import { InstanceResponseDto } from 'src/instances/dto/instance-response.dto';
+import { InstanceConnectionConfigResponseDto } from 'src/instances/dto/instance-connection-config-response.dto';
 
-export { CreateInstanceDto, InstanceResponseDto };
+export {
+  CreateInstanceDto,
+  InstanceResponseDto,
+  InstanceConnectionConfigResponseDto,
+};
